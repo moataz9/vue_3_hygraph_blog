9b1ed553bc0af7ea1866acefd5c82f28eaaa2dd4
moataz9 <moatazsamir9.in.khir99@gmail.com>
2022-10-04 17:07:20 +0200

    Author Componet

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
-import { PostContent, PostWidget, CategoriesWidget } from '@/components'
+import { PostContent, PostWidget, CategoriesWidget, Author } from '@/components'
 import { getPostContent } from '@/serives'
 import { onMounted, ref, watch } from 'vue'
-import type { post } from '@/types';
+import type { author, post } from '@/types'
 
 const route = useRoute()
 const currentSlug = route.params.slug as string
 
-const post = ref<post | undefined>(undefined);
+const post = ref<post | undefined>(undefined)
 
 onMounted(() => {
   getPostContent(currentSlug).then(({ result }) => {
@@ -24,7 +24,8 @@ onMounted(() => {
   <div class="container mx-auto only:sm:px-10 mb-8">
     <div class="grid grid-cols-1 lg:grid-cols-12 gap-12">
       <div class="col-span-1 lg:col-span-8">
-        <PostContent :post="post"/>
+        <PostContent :post="post" />
+        <Author :author="post?.author" />
       </div>
       <div class="col-span-1 lg:col-span-4">
         <div class="relative lg:sticky top-8">
